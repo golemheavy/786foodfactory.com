b5db3752a63c91772d4808692cf5775c6fa5d689
Steve <steve@gridtop.com>
2020-12-20 10:28:49 -0600

    importing from existing project

---
+++ assets/data/appetizers.js
@@ -0,0 +1,47 @@
+const appetizers = [
+	{
+		"name": "Vegetable Samosa",
+		"description": "Deep-fried savory pastry stuffed with masala (Indian spice mix) potatoes and onions.",
+		"price": 1.99
+	},
+	{
+		"name": "Two Samosas (Chicken or Beef)",
+		"description": 'Deep-fried savory pastry stuffed with minced chicken/beef cooked with assorted Indian spices ("keema samosa").',
+		"price": 3.99
+	},
+	{
+		"name": "Chicken 65",
+		"description": "Tender pieces of spice-marinated chicken deep-fried and then sauteed in more spices.",
+		"price": 9.99,
+		"spicy": true
+	},
+	{
+		"name": "Chilli Chicken",
+		"description": "Tender pieces of chicken sauteed in spicy chillies and tongue-searing spices.",
+		"price": 9.99,
+		"spicy": true
+	},
+	{
+		"name": "Chilli Paneer",
+		"description": "Tender pieces of paneer (Indian cottage cheese) sauteed in spicy chillies and tongue-searing spices.",
+		"price": 9.99,
+		"spicy": true
+	},
+	{
+		"name": "Paneer 65",
+		"description": "Tender pieces of spice-marinated paneer (Indian cottage cheese) deep-fried and then sauteed in more spices.",
+		"price": 9.99,
+		"spicy": true
+	},
+	{
+		"name": "Mughlai Paratha",
+		"description": "Crisp, flaky flatread stuffed with minced chicken seasoned with exquisite Indian spices.",
+		"price": 12.99
+	},
+	{
+		"name": "Mirchi Paratha",
+		"description": "Crisp, flaky flatbread stuffed with tongue-tickling mixture of green chillies and Indian spices.",
+		"price": 9.99,
+		"spicy": true
+	}
+];
---
+++ assets/data/kababsAndGrill.js
@@ -0,0 +1,104 @@
+const kababsAndGrill = [
+	{
+		"name": "Chicken Tikka (Leg)",
+		"description": "Chicken leg quarter marinated in a mouthwatering blend of spices and yogurt and barbequed to perfection.",
+		"price": 5.99
+	},
+	{
+		"name": "Chicken Tikka (Breast)",
+		"description": "Succulent chicken breast marinated in an exquisite blend of spices and yogurt and barbequed to perfection.",
+		"price": 6.99
+	},
+	{
+		"name": "Malai Chicken Boti",
+		"description": "Succulent pieces of chicken breast marinated in mild spices and barbequed to perfection.",
+		"price": 11.99
+	},
+	{
+		"name": "Masala Chicken Boti",
+		"description": "Succulent boneless chicken marinated in tongue-searing spices and barbequed to perfection.",
+		"price": 11.99,
+		"spicy": true
+	},
+	{
+		"name": "Chicken Boti",
+		"description": "Barbecued boneless chicken marinated in exotic spices.",
+		"price": 11.99
+	},
+	{
+		"name": "Seekh Kabab (Beef or Chicken)",
+		"description": "Ground beef or chicken mixed with fresh hebs and spices, shaped into cylindrical kababs and grilled to perfection.",
+		"price": 9.99
+	},
+	{
+		"name": "Seekh Kabab (Goat)",
+		"description": "Ground goat meat mixed with fresh hebs and spices, shaped into cylindrical kababs and grilled to perfection.",
+		"price": 14.99
+	},
+	{
+		"name": "Tangri Kabab",
+		"description": "Chicken drumsticks marinated in a blend of tongue-searing spices and barbequed to perfection.",
+		"price": 12.99,
+		"spicy": true
+	},
+	{
+		"name": "Bihari Kabab (Beef or Chicken)",
+		"description": "Beef/boneless chicken marinated in fresh herbs and roasted to perfection and then thinly sliced.",
+		"price": 12.99
+	},
+	{
+		"name": "Chapli Kabab (Beef or Chicken)",
+		"description": "Beef/chicken patty seasoned with a blend of Indian spices and grilled to perfection.",
+		"price": 11.99
+	},
+	{
+		"name": "Chapli Kabab Goat",
+		"description": "Ground goat patty seasoned with a blend of Indian spices and grilled to perfection.",
+		"price": 14.99
+	},
+	{
+		"name": "Charga (Chicken)",
+		"description": "Whole chicken cut into eight pieces, a blend of spices and grilled to perfection in a tandoor (oven).",
+		"price": 21.99
+	},
+	{
+		"name": "Lamb Chops",
+		"description": "Lamb chops marinated in a spicy blend and grilled to perfection.",
+		"price": 19.99
+	},
+	{
+		"name": "Afghani Malai Chicken Boti",
+		"description": "Barbequed slices of chicken breast seasoned with mild spices and cooked in a thick gravy.",
+		"price": 11.99
+	},
+	{
+		"name": "Tandoori Shrimp",
+		"description": "Succulent shrimp marinated in a blend of spices and grilled to perfection in a tandoor (oven).",
+		"price": 14.99
+	},
+	{
+		"name": "Tandoori Tilapia",
+		"description": "Whole tilapia fish marinated in an exquisite blend of spices and grilled to perfection in a tandoor.",
+		"price": 12.99
+	},
+	{
+		"name": "Fried Snapper",
+		"description": "Whole snapper fish marinated in a tantalizing blend of spices and deep-fried.",
+		"price": 28.99
+	},
+	{
+		"name": "Fried Pomfret",
+		"description": "Whole pompret fish marinated in a delectable blend of spices and deep-fried.",
+		"price": 19.99
+	},
+	{
+		"name": "Mixed Grill",
+		"description": "A combination of chicken seekh kabab, chicken boti, beef seekh kabab, and beef chapli.",
+		"price": 25.99
+	},
+	{
+		"name": "Mixed Grill Family Platter",
+		"description": "A combination of chicken seekh kabab, chicken boti, chicken chapli, beef seekh kabab, beef Bihari kabab, and beef seekh kabab.",
+		"price": 55.99
+	}
+];
